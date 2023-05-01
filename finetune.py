819fa01a358c061a1799e84331647869e805b6a1
Joe Ksiazek <joe@helmux.com>
2023-04-30 23:19:19 -0400

    save limit

--- finetune.py
+++ finetune.py
@@ -233,7 +233,7 @@ def train(
             eval_steps=eval_steps if val_set_size > 0 else None,
             save_steps=save_steps,
             output_dir=output_dir,
-            save_total_limit=1,
+            save_total_limit=3,
             load_best_model_at_end=True if val_set_size > 0 else False,
             ddp_find_unused_parameters=False if ddp else None,
             group_by_length=group_by_length,
