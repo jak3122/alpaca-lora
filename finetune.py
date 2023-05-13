62e6f9f2354f0b264cbf3e541d2bbf69b624602d
Joe Ksiazek <joe@helmux.com>
2023-05-13 15:44:45 -0400

    val size check

--- finetune.py
+++ finetune.py
@@ -94,7 +94,8 @@ def train(
         base_model
     ), "Please specify a --base_model, e.g. --base_model='huggyllama/llama-7b'"
     gradient_accumulation_steps = batch_size // micro_batch_size
-    eval_accumulation_steps = eval_batch_size // val_set_size
+    if val_set_size > 0:
+        eval_accumulation_steps = eval_batch_size // val_set_size
 
     device_map = "auto"
     world_size = int(os.environ.get("WORLD_SIZE", 1))
